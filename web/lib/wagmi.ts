129a406a84e61334f8d3701d38d4c695c9260773
easyweb3 <dev@easyweb3.tools>
2026-02-06 17:42:28 +0800

    fix bug

--- web/lib/wagmi.ts
+++ web/lib/wagmi.ts
@@ -3,7 +3,7 @@ import { bsc } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'EasyMeme',
-  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID || '',
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID || 'demo',
   chains: [bsc],
   ssr: true,
 });
